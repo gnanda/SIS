654928c121e823abaa3562a2067bd53ddd135b3d
gnanda <gnanda@mines.edu>
2013-11-07 22:04:06 -0700

    Adding files

---
+++ compare_index.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm setup_costs.dat
+rm shortage_costs.dat         
+rm holding_costs.dat          
+rm dependent_costs.dat
+
+
+
+for i in {1..40}; do
+    output=`./SIM C $i sis1.dat 33 | grep OUTPUT | awk '{print $2}'`
+
+    values=( $output )
+
+    echo ${values[0]} >> setup_costs.dat
+    echo ${values[1]} >> shortage_costs.dat
+    echo ${values[2]} >> holding_costs.dat
+    echo ${values[3]} >> dependent_costs.dat
+done
